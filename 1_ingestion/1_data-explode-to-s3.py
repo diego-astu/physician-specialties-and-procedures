0ed3575ad4153b9ad058127e9df34f223787be24
jdastudillo <jdiego@umich.edu>
2020-01-23 10:14:32 -0500

    Got small data in pandas and to s3 via EC2
    Biggest blocker was aws credentials (getting that set up on EC2)

---
+++ 1_ingestion/1_data-explode-to-s3.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python3
+
+
+
+# make sure to install these packages before running:
+# pip3 install pandas
+# pip3 install sodapy;
+# pip3 install numpy;
+# pip3 install boto3;
+# pip3 install fastparquet;
+# pip3 install s3fs;
+
+import pandas as pd
+import numpy as np
+from sodapy import Socrata
+import boto3
+from botocore.exceptions import NoCredentialsError
+from fastparquet import write
+
+
+
+# Unauthenticated client only works with public data sets. Note 'None'
+# in place of application token, and no username or password:
+medicare_client = Socrata("data.medicare.gov", None)
+cms_client = Socrata("data.cms.gov", None)
+
+
+####################################################################
+## TODO: CONVERT TO SPARK AND READ IN ENTIRE DATASET
+## WILL HAVE TO AUTHENTICATE TO PREVENT THROTTLING
+####################################################################
+## # MyAppToken = 'FakeCMSToken'
+# FakePW = '235536'
+# medicare_client = Socrata("data.medicare.gov",
+#                  MyAppToken,
+#                  username="jdiego@umich.edu",
+#                  password="FakePW")
+# cms_client = Socrata("data.cms.gov",
+#                  MyAppToken,
+#                  username="jdiego@umich.edu",
+#                  password="FakePW")
+
+
+
+###read in raw data (lists of dicts) via get method of SODA API
+#The Physician Information dataset is available at
+#https://data.medicare.gov/Physician-Compare/Physician-Compare-National-Downloadable-File/mj5m-pzi6
+phys_results = cms_client.get("fs4p-t5eq", limit=2*1000)
+#The Physician Utilization dataset is available at
+#https://dev.socrata.com/foundry/data.cms.gov/fs4p-t5eq
+util_results = medicare_client.get("2bhu-tmkt", limit = 2*1000)
+
+
+
+# Convert to pandas DataFrame AND MERGE
+phys = pd.DataFrame.from_records(phys_results)
+util = pd.DataFrame.from_records(util_results)
+merged_df = pd.merge(phys,util,on="npi")
+#list_of_counts = [i * 1 for i in  list(merged_df['line_srvc_cnt'].tolist())]
+
+#exploded_df = pd.DataFrame(np.repeat(merged_df.values,list_of_counts,axis=0))
+
+#exploded_df = pd.DataFrame(merged_df.values.repeat(merged_df.line_srvc_cnt, axis=0), columns=merged_df.columns)
+exploded_df = merged_df.reindex(merged_df.index.repeat(merged_df.line_srvc_cnt))
+
+
+
+s3_url = 's3://diego-bucket0/try0/test0/remote_exploded1_notsubsetted.parquet'
+exploded_df.to_parquet(s3_url, compression='gzip')
+
